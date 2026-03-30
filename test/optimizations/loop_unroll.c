b4321440f8da7a898498da05800ac82fdcf835de
adithya-ananth <adithyaananth2005@gmail.com>
2026-03-30 14:53:34 +0530

    updated test case

--- test/optimizations/loop_unroll.c
+++ test/optimizations/loop_unroll.c
@@ -2,9 +2,7 @@ int main() {
     int sum = 0;
     int i = 0;
     
-    // We loop exactly 10 times.
-    // Unrolling by a factor of 2 means the loop will only 
-    // evaluate the "i < 10" condition 5 times!
+    // We loop exactly 10 times. Unrolling by a factor of 2 means the loop will only evaluate the "i < 10" condition 5 times
     while (i < 10) {
         sum = sum + i;
         i = i + 1;
